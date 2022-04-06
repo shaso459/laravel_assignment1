7c01ab44e97bf62f89f44aeac3dc416c4c6e60df
Shaso Jalal <shasojalal1@gmail.com>
2022-04-06 22:03:05 +0300

    installation

---
+++ resources/views/biography.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div><h1>My name is shaso and 21 yers old and i study at information tecnology department on tci collage</h1></div>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="/images/dv.jpg">
+    <div>
+        <h1>Name: shaso jalal muheden</h1>
+        <h2>Age: 21 y</h2>
+        <h3>Birthdate: 30/11/2001</h3>
+        <h4>Phone: 07704521743</h4>     
+    </div>
+    
+    
+</body>
+</html>
